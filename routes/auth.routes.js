3811569cb8775e7dc60b05c5832ab0f56ebb1364
pranjali0505 <pranjaljagtap147@gmail.com>
2023-12-31 15:35:43 +1400

    changes

--- routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const router=require('express').Router();
-const authController=require('../controller/auth.controller');
-
-router.post('/login',authController.loginUser);
-
-
-module.exports=router;
